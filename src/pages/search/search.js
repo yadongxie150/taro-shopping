924a77ecb36b6e497842b9d064b5e71f42273e2c
xieyadong <xieyadong@xiaomi.com>
2019-12-16 23:24:16 +0800

    update

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -1,5 +1,5 @@
 import Taro, { Component } from '@tarojs/taro'
-import { View, Button, Text, Image } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import { connect } from '@tarojs/redux'
 
 import { add } from '../../actions/counter'
@@ -31,9 +31,53 @@ class Search extends Component {
   componentDidHide() { }
 
   render() {
+    const progress = [
+      '打开京东APP',
+      '复制商品标题',
+      '打开好物清单',
+      '点击搜索',
+    ]
     return (
-      <View className='goodDetail'>
-        搜索详情页
+      <View className='search'>
+        <View className="search-header">搜索头部组件</View>
+        <View className="search-body">
+          <View className="search-progress">
+            <View className="search-progress-header">京东100%的商品都有优惠券或返利</View>
+            <View className="search-progress-body">
+              {
+                progress.map((des, index) => (
+                  <View className="search-progress-item">
+                    <View className="search-progress-item-left">
+                      <View className="search-progress-item-left-num">{index}</View>
+                      <View className="search-progress-item-left-des">{des}</View>
+                    </View>
+                    {index < 3 && <View className="search-progress-item-right"></View>}
+                  </View>
+                ))
+              }
+            </View>
+          </View>
+          <View className="search-exp">
+            <View className="search-exp-header">
+              <Text>最近搜索</Text>
+              <View className="search-exp-clear">清除</View>
+            </View>
+            <View className="search-exp-body">
+              <View className="search-exp-body-item">耳机</View>
+              <View className="search-exp-body-item">手机更新迭代</View>
+            </View>
+          </View>
+          <View className="search-exp">
+            <View className="search-exp-header">
+              <Text>热门搜索</Text>
+              <View className="search-exp-clear">清除</View>
+            </View>
+            <View className="search-exp-body">
+              <View className="search-exp-body-item">耳机</View>
+              <View className="search-exp-body-item">手机更新迭代</View>
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
